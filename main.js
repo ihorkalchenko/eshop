02f9ad90b68cb8b54f9f13f3e6efda6a005395ef
ihorkalchenko <ihorka88@gmail.com>
2019-01-23 17:25:28 +0200

    001: Add breadcrumb component

--- main.js
+++ main.js
@@ -1,9 +1,144 @@
 
+// header
+Vue.component('app-header', {
+    props: ['logo'],
+
+    template: `
+        <header class="header">
+            <div class="container">
+                <a href="/" class="logo" v-html="logo"></a>
+
+                <slot></slot>
+            </div>
+        </header>
+    `
+});
+
+// footer
+Vue.component('app-footer', {
+    template: `
+        <footer class="footer">
+            <div class="container">
+                <p>&copy; {{ copyright }} Eshop. The experimental product page.</p>
+            </div>
+        </footer>
+    `,
+
+    computed: {
+        copyright() {
+            return new Date().getFullYear();
+        }
+    }
+});
+
+// cart
+Vue.component('app-cart', {
+    props: ['cart'],
+
+    template: `
+        <div class="cart">
+            <span class="cart-icon"></span>
+            <transition name="show-up" mode="out-in">
+                <span class="cart-text" :key="cart">{{ cart }}</span>
+            </transition>
+        </div>
+    `
+});
+
+// breadcrumb
+Vue.component('app-breadcrumb', {
+    template: `
+        <nav class="breadcrumb">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li class="is-active"><a href="#">Apple Sillicon Case</a></li>
+            </ul>
+        </nav>
+    `
+});
+
+// product
+Vue.component('app-product', {
+    template: `
+        <div class="product">
+            <div class="product-image">
+                <div class="full-image">
+                    <div class="full-image-inner">
+                        <img :src="image" />
+                    </div>
+                </div>
+            </div>
+            <div class="product-detail">
+                <div class="product-content">
+                    <div class="product-title">
+                        <h1>{{ product }}</h1>
+                    </div>
+
+                    <div class="product-sku">
+                        <p>SKU: {{ sku }}</p>
+                    </div>
+
+                    <div class="product-price">
+                        <p>{{ priceFilter }}</p>
+                    </div>
+
+                    <div class="product-stock">
+                        <p>{{ inStock ? 'In Stock' : 'Out of stock' }}</p>
+                    </div>
+
+                    <div class="product-description">
+                        <p>{{ description }}</p>
+                    </div>
+
+                    <button type="button" class="btn is-green" @click="addToCart">Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    `,
+
+    data() {
+        return {
+            product: 'iPhone Case',
+            description: 'Designed for iPhone 7 / iPhone 8, this sillicone case fits snugly over the volume buttons, side button and curves of your device without adding bulk',
+            image: 'https://images-na.ssl-images-amazon.com/images/I/419rCJZ2xfL._SL1024_.jpg',
+            inStock: true,
+            price: 15,
+            sku: 12345678
+        }
+    },
+
+    methods: {
+        addToCart() {
+            this.$emit('add-to-cart');
+        }
+    },
+
+    computed: {
+        priceFilter: function() {
+            return '$' + this.price.toFixed(2);
+        }
+    }
+});
+
 
 var app = new Vue({
     el: '#app',
     
     data: {
-        title: 'Eshop'
+        title: 'Eshop',
+        cart: 0
+    },
+
+    methods: {
+        updateCart() {
+            this.cart += 1
+        }
+    },
+
+    computed: {
+        logo() {
+            return '<b>' + this.title[0] + '</b>' + this.title.slice(1);
+        }
     }
 });
